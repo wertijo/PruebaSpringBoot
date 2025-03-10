8841563f7e5677e6cdcf2190ccf24b7fcfe1fd09
Alejandro <alejoreyes124@gmail.com>
2025-03-10 10:53:51 -0500

    aplicando mvc para obtener un usuario

--- src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -3,11 +3,19 @@ package com.cursojava.curso.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cursojava.curso.models.Usuario;
+
 @RestController
 public class UsuarioController {
 
-    @RequestMapping(value = "prueba")
-    public String prueba(){
-        return "prueba";
+    @RequestMapping(value = "usuario")
+    public Usuario getUsuario(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Alejandro");
+        usuario.setApellido("Reyes");
+        usuario.setEmail("alejo@gmail.com");
+        usuario.setTelefono("1234");
+        usuario.setPassword("4321");
+        return usuario;
     }
 }
